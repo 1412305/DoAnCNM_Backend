3c2713a19351fb47a15dc7ff8ccad5d6995783fd
nhatnt <nhatnt@vng.com.vn>
2018-01-10 10:56:11 +0700

    [Admin] get Address Info

--- controllers/address-controller.js
+++ controllers/address-controller.js
@@ -4,25 +4,34 @@ var User = mongoose.model('User');
 var passwordHash = require('password-hash');
 
 exports.listAddresses = function(req, res) {
-    responseData = [];
+    let responseData = [];
     if (req.decoded.authority != "admin")
         return;
     Address.find({},function(err, result) {
         if (err) {
             res.send(err);
         }
-        var index = 0;
+        let index = 0;
         result.forEach(function(element) {
             User.findById(element.ofUser, function(err, user){
-                index++;
-                responseData.push({
-                    address: element.addressName,
-                    user: user.email,
-                    authority: user.authority
-                });
-                if (index == result.length) {
-                    res.json(responseData);
-                }
+              index++;
+              let addressInfo = {
+                address: element.addressName,
+                user: "",
+                availableBalance: 0,
+                actualBalance: 0
+              }
+              if (user) {
+                addressInfo.user = user.email
+                addressInfo.availableBalance = user.availableBalance;
+                addressInfo.actualBalance = user.actualBalance;
+              }
+              responseData.push(addressInfo);
+              if (index === result.length)
+              {
+                console.log(responseData)
+                res.json(responseData)
+              }
             })
         })
     });
